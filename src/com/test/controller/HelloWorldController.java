732b1a0461323ef3c7d19e1f0946da69f0a834d9
wy666888 <flab100@126.com>
2019-01-05 15:07:57 +0800

    start

---
+++ src/com/test/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.test.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloWorldController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("=======springmvc======");
+		return new ModelAndView("helloWorld");
+	}
+
+}
